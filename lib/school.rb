ea5e11c13e7df229abfff37cd577b10b88567b0f
Nathan Miller <natem31190@gmail.com>
2021-01-08 20:23:14 -0500

    commit tree 73ffe3356fc384d05c2cf25714d50687abd5c997

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
 # code here!
+class School
+    attr_accessor :roster
+    attr_reader :name
+    def initialize(name)
+         @name = name
+         @roster = {}
+    end
+
+    def add_student(student_name, grade)
+        if @roster.has_key?(grade)
+            @roster[grade] << student_name
+        else 
+            @roster[grade] = []
+            @roster[grade] << student_name
+        end   
+    end
+
+    def grade(grade_number)
+        @roster[grade_number]
+
+    end
+
+    def sort
+    @roster.each do |grade, students|
+        students.sort!
+    end
+    end
+end
+
